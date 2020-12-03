0b6aef2fedad0b65e867c91134e049cc0af62d7c
MichielGeyskens <michielgeyskens@hotmail.be>
2020-12-03 13:25:27 +0100

    Day 3

--- days/3/index.ts
+++ days/3/index.ts
@@ -1,8 +1,24 @@
-let answer: any[] = [];
-function solve(input: number[]) {
+let answer:number[]=[];
+function solve(input: string[]) {
+  let list:number[][]=[[1,1],[3,1],[5,1],[7,1],[1,2]];
+  list.forEach((element,index)=>{
+    const [x,y]=element;
+    let inputs=input;
+    if(y==2){
 
-  answer.push();
-  return answer;
+      inputs =input.filter(function(v, i) {
+        // check the index is odd
+        return i % 2 == 0;
+      });
+    }
+  
+    answer.push(0);
+    inputs.forEach((el,i)=> (el.charAt(i*x%el.length)=="#")?answer[index]++:"");
+    
+    
+  });
+  console.log(answer);
+  return answer.reduce((a:number, b:number)=> a*b, 1);
 }
 
 
--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import fs from "fs";
-const day = 2;//(new Date).getDay();
+const day = 3;//(new Date).getDay();
 
-import solve from "./days/2/index";// needs to be dynamic
+import solve from "./days/3/index";// needs to be dynamic
 
 const input: Array<any>  = fs
   .readFileSync(`days/`+day+`/input.txt`)
   .toString()
   .split("\n")
-  .map((s) => s.replace(/\r$/, ""))
-  .filter((s) => s.length > 0);
+  .map((s) => s.replace(/\r$/, ""));
+  // .filter((s) => s.length > 0);
   // .map(s=>parseInt(s));
 
 // const solver = require(`./days/`+day+`/index.js`);
