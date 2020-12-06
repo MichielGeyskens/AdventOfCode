eab06353cddcf68487a30b2569a0481c530afef5
MichielGeyskens <michielgeyskens@hotmail.be>
2020-12-06 12:45:13 +0100

    Day 6 part 2

--- days/6/index.ts
+++ days/6/index.ts
@@ -1,13 +1,26 @@
 let answer: any[] = [];
 let checklist: boolean[] = new Array(26).fill(false);
 function solve(input: string[]) {
+    let indexperson=0;
     input.forEach(el => {
         if (el != "") {
-            el.split('').forEach((char, index) => {
-                checklist[el.charCodeAt(index) - 97] = true;
-            })
+            if(indexperson==0){
+                el.split('').forEach((char, index) => {
+                    checklist[el.charCodeAt(index) - 97] = true;
+                })
+            }
+            else{
+                checklist.forEach((element,index) => {
+                    if(element){
+                        if(-1==el.indexOf(String.fromCharCode(97 + index)))
+                        checklist[index]=false;
+                    }
+                });
+            }
+            indexperson++;
         }
         else {
+            indexperson=0;
             answer.push(countTrue(checklist))
             checklist = new Array(26).fill(false);
         }
